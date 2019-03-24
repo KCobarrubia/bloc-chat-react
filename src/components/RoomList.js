ee98735f31c48f0595f17d18a86297d97be3bfd8
Kevin Cobarrubia <cobarrubiakevin@yahoo.com>
2019-03-23 17:05:54 -0700

    Checkpoint 03 Create Chat Rooms: Completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,9 +4,12 @@ class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName:''
     };
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.onChange = this.onChange.bind(this);
+    this.handleClickSubmit = this.handleClickSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -15,6 +18,16 @@ class RoomList extends Component {
       room.key = snapshot.key;
       this.setState({ rooms: this.state.rooms.concat(room) })
     });
+  }
+
+  onChange(e) {
+    this.setState({ newRoomName: e.target.value});
+  }
+
+  handleClickSubmit() {
+    this.roomsRef.push(
+      { name: this.state.newRoomName}
+    );
   }
 
   render() {
@@ -27,6 +40,29 @@ class RoomList extends Component {
             )
           }
         </ul>
+
+        <button type="button" data-toggle="modal" data-target="#myModal">Create Room</button>
+        <div className="modal" id="myModal">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h4 className="modal-title">Create Room</h4>
+                <button type="button" className="close" data-dismiss="modal">&times;</button>
+              </div>
+
+              <div className="modal-body">
+                <form onSubmit={this.handleClickSubmit}>
+                  <p>Enter a room name: </p>
+                  <input type="text" value={this.state.newRoomName} onChange={this.onChange} />
+                  <input type="submit" value="Submit" />
+                </form>  
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-danger" data-dismiss="modal">Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
